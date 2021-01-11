b4bc3bbd614f390a4a4a78c401fb0a15977e6879
iyrenepsam <irenepsam@gmail.com>
2021-01-11 14:13:12 +0530

    chanded to implement modules

--- largest.js
+++ largest.js
@@ -1,13 +1,30 @@
-var x=20
-var y=200
-var z= 350
-if(x>y && x>z){
-    console.log(x);
+   
+   
+module.exports.largest=(x,y,z)=>
+{
+    if(x>y&&x>z){
+        return x
+    }
+    else if(y>z)
+    {
+        return y
+    }
+    else 
+    {
+       return z
+    }
+}
+module.exports.smallest=(x,y,z)=>{
+    if(x<y&&x<z){
+        return x
+    }
+    else if(y<z)
+    {
+        return y
+    }
+    else
+    {
+       return z
+    }
+}
 
-}
-else if(y>z){
-    console.log(y);
-}
-else{
-    console.log(z);
-}
