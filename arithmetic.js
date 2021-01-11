f0139b991b1bcb48b5549eea1d139dff5e998115
iyrenepsam <irenepsam@gmail.com>
2021-01-11 14:18:03 +0530

    pushing arithmetic calculations

---
+++ arithmetic.js
@@ -0,0 +1,19 @@
+module.exports.addNum = (x,y) => {
+    var c=x+y
+    return c
+}
+
+module.exports.subNum =(x,y)=>
+{
+    var c= x-y
+    return c
+}
+module.exports.mulNum=(x,y)=>{
+    var c=x*y
+    return c
+}
+module.exports.divNum=(x,y)=>
+{
+    var c=x/y
+    return c
+}
