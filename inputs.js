df420762df2014914fc593de5be565ae40456dba
iyrenepsam <irenepsam@gmail.com>
2021-01-11 14:20:17 +0530

    calling arithmetic prgm in modules

---
+++ inputs.js
@@ -0,0 +1,14 @@
+const entery = require("./arithmetic")
+
+var x = 4
+var y = 8
+
+var a = entery.addNum(x,y)
+var s = entery.subNum(x,y)
+var m = entery.mulNum(x,y)
+var d = entery.divNum(x,y)
+
+console.log(a)
+console.log(s)
+console.log(m)
+console.log(d)
